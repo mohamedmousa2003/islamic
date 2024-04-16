b7389560833876c8e9bfa10b01b66e26ebc6701e
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-04-16 22:29:16 +0200

    ADD Home Screen

--- lib/pages/home_screen/home_screen.dart
+++ lib/pages/home_screen/home_screen.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   static String routeName = "home";
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage("assets/light_images/background.png"),
+        ),
+      ),
+      child: Scaffold(),
+    );
   }
 }
