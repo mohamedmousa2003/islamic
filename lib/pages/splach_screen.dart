bf502bd651bf8dbc04178c688071700d9f1e6531
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-04-16 21:42:56 +0200

    first commit

---
+++ lib/pages/splach_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget{
+  static String routName = "splash";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
